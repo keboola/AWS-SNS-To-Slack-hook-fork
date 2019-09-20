c7c21f8fa07ad69af30b6a43756c0ab80a748de8
Tomas Kacur <tomas.kacur@keboola.com>
2019-09-20 12:10:23 +0200

    deploy to us-east-2

--- gulpfile.js
+++ gulpfile.js
@@ -95,6 +95,10 @@ gulp.task('upload-eu', function () {
   return upload('eu-central-1');
 });
 
+gulp.task('upload-us-east-2  ', function () {
+  return upload('us-east-2');
+});
+
 // The key to deploying as a single command is to manage the sequence of events.
 gulp.task('default', function(callback) {
   return runSequence(
